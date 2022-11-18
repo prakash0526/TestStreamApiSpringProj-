377d919fa569b82443ae3a3341a7dcca7b4257e2
unknown <prakash0526>
2022-11-18 15:56:40 +0530

    added new project

---
+++ src/test/java/com/test/teststreamapispringproj/TestStreamApiSpringProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.teststreamapispringproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestStreamApiSpringProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
